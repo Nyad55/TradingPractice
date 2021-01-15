6811bf7b9b9569f3acdc8cead65418f701b86a68
Jason Ward <nyad55@gmail.com>
2021-01-15 23:48:54 +0200

    added enums.py

--- main.py
+++ main.py
@@ -10,42 +10,8 @@ import glob
 import math
 import pygame
 from pygame.locals import *
-from enum import Enum
+from enums import TradeMode, TradeType, TradeState, Stats, OHLC
 
-class TradeMode(Enum):
-    """Enum for selecting the mode of the trade"""
-    BUY = 1
-    SELL = 2
-    CLOSED = 3
-
-class TradeType(Enum):
-    """Enum for selecting what type of trade is placed"""
-    TREND = 1
-    FADE = 2
-
-class TradeState:
-    """"Keeps track of the current state"""
-    equity = 100
-    profit = 0
-    trade_mode = TradeMode.CLOSED
-    order_price = 0
-    position_size = 0
-    stop_loss_price = 0
-    pips = 0
-    trade_type = TradeType.TREND
-    candle_number = 0
-
-class Stats:
-    """Tracks the different types of trade pip wins and losses for statistical analysis"""
-    trend = []
-    fade = []
-
-class OHLC(Enum):
-    """Enum for picking Open High Low Close data from the bid and ask lists"""
-    OPENINDEX = 1
-    HIGHINDEX = 2
-    LOWINDEX = 3
-    CLOSEINDEX = 4
 
 DATAFOLDER = "data"
 CHARTPIPHEIGHT = 200
